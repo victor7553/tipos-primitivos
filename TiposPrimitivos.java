650862829c360cd198fa00bd8060223acbbdc09f
Victor Hugo <69818265+victor7553@users.noreply.github.com>
2023-03-08 21:12:10 -0300

    adicionando arquivo

---
+++ TiposPrimitivos.java
@@ -0,0 +1,27 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+	public static void main(String[] args) {
+		
+		//tipo inteiro	
+		
+		byte anosDaEmpresa = 127;
+		short numeroDeVoos = 3213;
+		int id = 42342;
+		long margemDeLucro = 2_343_876_223L;
+		
+		//tipos reais
+		
+		float salario = 10.44F;
+		double vendasAcumuladas = 10.44;
+		
+		//tipo booleano
+		
+		boolean recesso = false;
+		
+		//tipo caractere
+		
+		char status = 'A';
+	}
+
+}
